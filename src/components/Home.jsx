2bc0dfac9b4c95ffa98afb5f63d4cdcfec68d4d8
tahielpedevilla <tahielpedevilladedonato@gmail.com>
2021-09-13 21:00:37 -0300

    modifying routes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,23 +2,35 @@ import React from "react";
 import {Box, Flex, Heading, Stack} from "@chakra-ui/layout";
 import {Text, Image} from "@chakra-ui/react";
 
+const BACKGROUND2 =
+  "https://images.unsplash.com/photo-1489269637500-aa0e75768394?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80";
 const BACKGROUND =
-  "https://images.unsplash.com/photo-1505843490538-5133c6c7d0e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80";
+  "https://images.unsplash.com/photo-1598300056393-4aac492f4344?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=460&q=80";
 const Home = () => {
   return (
     <>
-      <Box h="100%" m={16} rounded={"lg"}>
-        <Heading p={8} textAlign="center">
+      <Box h="100%" m={16}>
+        <Heading fontSize="7xl" p={8} textAlign="center" textShadow="dark-lg">
           Welcome to Walla´s
         </Heading>
+        <Text fontSize="2xl" textAlign="center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, natus debitis? Nisi enim, natus molestiae
+          voluptatem dolor quidem eos numquam.
+        </Text>
         <Flex flexWrap="wrap" justify="center">
-          <Stack direction="row" spacing={8}>
-            <Text fontSize="xl" textAlign="center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora optio harum, laboriosam ut libero quas
-              hic perspiciatis at, placeat perferendis asperiores quasi odio repellat ipsum facere distinctio maxime.
-            </Text>
+          <Stack direction="row" spacing={8} />
+          <Stack direction="row" m={16} spacing={6}>
+            <Image rounded={"xl"} src={BACKGROUND2} />
+            <Stack direction="column" spacing={8}>
+              <Image rounded={"xl"} src={BACKGROUND} />
+              <Image rounded={"xl"} src={BACKGROUND} />
+            </Stack>
           </Stack>
-          <Image mt={16} src={BACKGROUND} w="70%" />
+          <Text fontSize="xl" textAlign="center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora optio harum, laboriosam ut libero quas hic
+            perspiciatis at, placeat perferendis asperiores quasi odio repellat ipsum facere distinctio maxime. Lorem
+            ipsum dolor sit amet.
+          </Text>
         </Flex>
       </Box>
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import {useColorMode} from "@chakra-ui/react";
 import {Button, IconButton} from "@chakra-ui/button";
-import {Box, Flex, Heading, Link, Stack} from "@chakra-ui/layout";
+import {Box, Flex, Heading, Stack} from "@chakra-ui/layout";
 import {MoonIcon, SunIcon} from "@chakra-ui/icons";
+import {Link} from "react-router-dom";
+
+import Products from "./Products";
+import Contact from "./Contact";
 
 const NavBar = () => {
   const {colorMode, toggleColorMode} = useColorMode();
@@ -10,12 +14,12 @@ const NavBar = () => {
   return (
     <Box bg={colorMode === "light" ? "Dark" : "Light"} w="100%">
       <Flex boxShadow="lg" justify="space-around" p={4}>
-        <Heading>{"Walla's"}</Heading>
+        <Heading>{"Walla´s"}</Heading>
         <Stack direction="row" spacing={6}>
-          <Button as={Link} href="/products" variant="link">
+          <Button as={Link} to={Products} variant="link">
             Products
           </Button>
-          <Button as={Link} href="/contact" variant="link">
+          <Button as={Link} to={Contact} variant="link">
             Contact
           </Button>
           <IconButton icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />} onClick={toggleColorMode} />
